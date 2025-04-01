e7658497cc69a9c2d5b8cf988eb01003b2bc1fe9
Alexander Montero <alexmon2802@gmail.com>
2025-04-01 15:21:37 -0600

    Proyecto completo de Tienda hasta semana 12 Commit anteriores no se pudieron hacer por estos errores del NB

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -10,5 +10,18 @@ function readURL(input) {
         };
         reader.readAsDataURL(input.files[0]);
     }
+
 }
 
+/* La siguiente función se utiliza para activar la cantidad de elementos seleccionados
+* En el carrito de compras utilizando un llamado "ajax" */
+
+function addCard(formulario) {
+    
+    var valor = formulario.elements[0].value;
+    var url = '/carrito/agregar';
+    url = url + '/' + valor;
+    $("#resultsBlock").load(url);
+
+}
+ 
